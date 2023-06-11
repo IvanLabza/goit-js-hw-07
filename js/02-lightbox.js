0674e592a77887ba8d1999b05a8d3e5fcc070513
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2023-06-11 19:18:21 +0300

    main

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './galleryItems';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+  const galleryElement = document.querySelector('.gallery');
+  galleryItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('gallery__item');
+
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
+
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.alt = item.description;
+
+    link.appendChild(image);
+    listItem.appendChild(link);
+    galleryElement.appendChild(listItem);
+  });
+});
